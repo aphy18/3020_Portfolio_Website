442ab654a80378c52e88b9eea6f626757e7b7291
SABA HAILU <sabahailu@SABAs-MacBook-Air.local>
2024-08-12 15:16:52 -0400

    formatted blog page

--- scripts.js
+++ scripts.js
@@ -1,35 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
   const blogList = document.getElementById('blog-list');
-
   fetch('./posts.json') 
     .then(response => response.json()) 
     .then(posts => {
       posts.forEach(post => {
-        
         // create post container
         let postContainer = document.createElement('div');
         postContainer.classList.add('post-container');
-
         // create the post title first
         let postTitle = document.createElement('h1');
         postTitle.innerHTML = post.title;
         postTitle.classList.add('post-title');
-
-        // create post body
+        // create the post image
+        let postImg = document.createElement('img');
+        postImg.src = post.image;
+        if (post.id === 1) {
+          postImg.classList.add('post-image-left');
+        } else {
+          postImg.classList.add('post-image-right');
+        }
+        // create the post body
         let postBody = document.createElement('div');
         postBody.innerHTML = post.body;
         postBody.classList.add('post-body');
-        
-        const postElement = document.createElement('div');
-        postElement.innerHTML = post.body;
-        console.log('each post html:', postElement.innerHTML)
-        
         // append each post container to the blog section
         blogList.append(postContainer);
-
         // append each post to their post container
-        postContainer.append(postTitle, postBody);
-
+        postContainer.append(postTitle, postImg, postBody);
       });
     })
   .catch(error => console.error('Error loading blog posts:', error)); });
